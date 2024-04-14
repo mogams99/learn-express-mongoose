d86c704a8608bfe9ef869fc57d95e2405309abce
Mochammad Gamal <mogams1811@gmail.com>
2024-04-14 14:15:48 +0700

    chore: :construction: restful index products on project

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ mongoose.connect('mongodb://127.0.0.1/shop-db').then(res => {
     console.log(err);
 })
 
-app.set('view', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Routing
 app.get('/', (req, res) => {
     res.send(`Hello World!`);
+});
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
